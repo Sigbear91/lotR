df7cfcfe5b1b4e09595d8fc454cefda4232342ba
Sigbear91 <36767167+Sigbear91@users.noreply.github.com>
2018-02-23 16:09:38 +0100

    Lotr

--- R/lotR.R
+++ R/lotR.R
@@ -1,12 +1,12 @@
 #' Lord of the Rings soundboard
 #'
 #' Plays a quote from LotR upon execution
-#' @param x Clip. Defaults to "over".
+#' @param Clip Choose soundclip.  Defaults to "over". Possible values: "clever", "mellon", "no", "over", "shallnotpass", "shut", "task", "walls", "wizard", "work"
 #' @keywords Lotr
 #' @export
 #' @examples
 #' lotr("mellon")
 
-lotr <- function(x = "over"){
-  system(paste0('powershell -c (New-Object Media.SoundPlayer "',paste(.libPaths()),'\\lotR\\',x,'.wav").PlaySync();'))
+lotr <- function(clip = "over"){
+  system(paste0('powershell -c (New-Object Media.SoundPlayer "',paste(.libPaths()),'\\lotR\\',clip,'.wav").PlaySync();'))
 }
